c31620b19e5aed03b46520cd7414dd72131e2528
S.Trishaa <2022ebcs077@online.bits-pilani.ac.in>
2025-11-02 23:31:36 +0530

    Changed HelloWorld.py code

---
+++ src/main/python/helloworld.py
@@ -0,0 +1,12 @@
+
+"""
+This is a simple module to demonstrate a hello world function in
+Python.
+"""
+import sys
+def helloworld(out):
+
+
+out.write("Hello world of Python\n")
+# Call the function with a file object
+helloworld(sys.stdout)
